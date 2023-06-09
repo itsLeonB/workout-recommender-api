2283d2b093f8cb52e99470e27256bbf3a81aa3fd
Ellion Blessan <ellionblessan@gmail.com>
2023-06-09 20:39:12 +0700

    Update code

--- main.py
+++ main.py
@@ -1,56 +1,14 @@
-# README
-# Hello everyone, in here I (Kaenova | Bangkit Mentor ML-20) 
-# will give you some headstart on createing ML API. 
-# Please read every lines and comments carefully. 
-# 
-# I give you a headstart on text based input and image based input API. 
-# To run this server, don't forget to install all the libraries in the
-# requirements.txt simply by "pip install -r requirements.txt" 
-# and then use "python main.py" to run it
-# 
-# For ML:
-# Please prepare your model either in .h5 or saved model format.
-# Put your model in the same folder as this main.py file.
-# You will load your model down the line into this code. 
-# There are 2 option I give you, either your model image based input 
-# or text based input. You need to finish functions "def predict_text" or "def predict_image"
-# 
-# For CC:
-# You can check the endpoint that ML being used, eiter it's /predict_text or 
-# /predict_image. For /predict_text you need a JSON {"text": "your text"},
-# and for /predict_image you need to send an multipart-form with a "uploaded_file" 
-# field. you can see this api documentation when running this server and go into /docs
-# I also prepared the Dockerfile so you can easily modify and create a container iamge
-# The default port is 8080, but you can inject PORT environement variable.
-# 
-# If you want to have consultation with me
-# just chat me through Discord (kaenova#2859) and arrange the consultation time
-#
-# Share your capstone application with me! 🥳
-# Instagram @kaenovama
-# Twitter @kaenovama
-# LinkedIn /in/kaenova
-
-## Start your code here! ##
-
 import os
 import uvicorn
 import traceback
-import tensorflow as tf
+import pandas as pd
+import numpy as np
+import pickle
 
 from pydantic import BaseModel
-from urllib.request import Request
-from fastapi import FastAPI, Response, UploadFile
-from utils import load_image_into_numpy_array
-
-# Initialize Model
-# If you already put yout model in the same folder as this main.py
-# You can load .h5 model or any model below this line
-
-# If you use h5 type uncomment line below
-# model = tf.keras.models.load_model('./my_model.h5')
-# If you use saved model type uncomment line below
-# model = tf.saved_model.load("./my_model_folder")
+# from urllib.request import Request
+from fastapi import FastAPI, Response
+from sklearn.metrics.pairwise import cosine_similarity
 
 app = FastAPI()
 
@@ -59,62 +17,33 @@ app = FastAPI()
 def index():
     return "Hello world from ML endpoint!"
 
-# If your model need text input use this endpoint!
-class RequestText(BaseModel):
-    text:str
+class RequestId(BaseModel):
+    idx:int
 
-@app.post("/predict_text")
-def predict_text(req: RequestText, response: Response):
+@app.post("/load_encodings")
+def load_encodings():
+    file_path = "encodings.pickle"
+    with open(file_path, "rb") as pkl:
+        encodings = pd.DataFrame(pickle.load(pkl))
+    encodings_matrix = np.vstack(encodings["encodings"].to_numpy())    
+    return encodings, encodings_matrix
+
+@app.post("/get_recommendations")
+def get_recommendations(req: RequestId, response: Response, encodings, encodings_matrix):
     try:
-        # In here you will get text sent by the user
-        text = req.text
-        print("Uploaded text:", text)
-        
-        # Step 1: (Optional) Do your text preprocessing
-        
-        # Step 2: Prepare your data to your model
-        
-        # Step 3: Predict the data
-        # result = model.predict(...)
-        
-        # Step 4: Change the result your determined API output
-        
-        return "Endpoint not implemented"
+        workout_index = req.idx
+        workout_encoding = np.reshape(encodings.iloc[workout_index]['encodings'], (1, -1))
+        sim_scores = cosine_similarity(workout_encoding, encodings_matrix)[0]
+        sim_scores = pd.DataFrame(sim_scores, columns=['similarity_score'])
+        sim_scores = sim_scores.sort_values(by='similarity_score', ascending=False)
+        sim_scores = sim_scores.drop(workout_index)
+        top3 = sim_scores.head(3)
+        return top3.index
+    
     except Exception as e:
         traceback.print_exc()
         response.status_code = 500
         return "Internal Server Error"
-
-# If your model need image input use this endpoint!
-@app.post("/predict_image")
-def predict_image(uploaded_file: UploadFile, response: Response):
-    try:
-        # Checking if it's an image
-        if uploaded_file.content_type not in ["image/jpeg", "image/png"]:
-            response.status_code = 400
-            return "File is Not an Image"
-        
-        # In here you will get a numpy array in "image" variable.
-        # You can use this file, to load and do processing
-        # later down the line
-        image = load_image_into_numpy_array(uploaded_file.file.read())
-        print("Image shape:", image.shape)
-        
-        # Step 1: (Optional, but you should have one) Do your image preprocessing
-        
-        # Step 2: Prepare your data to your model
-        
-        # Step 3: Predict the data
-        # result = model.predict(...)
-        
-        # Step 4: Change the result your determined API output
-        
-        return "Endpoint not implemented"
-    except Exception as e:
-        traceback.print_exc()
-        response.status_code = 500
-        return "Internal Server Error"
-
 
 # Starting the server
 # Your can check the API documentation easily using /docs after the server is running
